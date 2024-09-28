9e985254d31af71e237e068eae875d953c232f9e
OR <oregev11@gmail.com>
2024-09-29 00:03:31 +0300

    dev

--- get_data.py
+++ get_data.py
@@ -1,13 +1,28 @@
 import argparse
 from crystoper import config
 
-def main():
-    parser = argparse.ArgumentParser(description="A script template using argparse")
+
+
+def parse_args():
     
-    parser.add_argument('-i', '--ids-path', type=str, default=, help='path to json with pdb ids list')
-    parser.add_argument('-o', '--output', type=str, required=True, help='Output file path')
+    parser = argparse.ArgumentParser(description="Parse get_data.py arguments")
+    
+    parser.add_argument('-i', '--ids-path', type=str, default=config.pdb_ids_path, help='Path to json with pdb ids list')
+    parser.add_argument('-o', '--output', type=str, default=config.downloaded_data_path, help='Path to save downloaded data in.')
+    parser.add_argument('-s', '--seq', type=str, action='store_true', help='Boolean flag for downloading the sequence of each id.')
+    parser.add_argument('-c', '--conditions', type=str, action='store_true', help='Boolean flag for downloading the the crystallization conditions\
+        for each id.')
+    
     
     args = parser.parse_args()
+    
+    return args
+    
+
+def main():
+
+    args = parse_args()
+    
 
 if __name__ == "__main__":
     main()
---
+++ pdb_db/data_downloader.py
@@ -0,0 +1,164 @@
+from time import sleep
+from os.path import exists
+import argparse
+import json
+import requests
+from Bio import SeqIO
+from io import StringIO
+import pandas as pd
+from tqdm import tqdm
+
+
+PDB_REST_API_DOWNLOAD_URL = "https://files.rcsb.org/download/"
+ENTRY_REST_API_DOWNLOAD_URL = "https://data.rcsb.org/rest/v1/core/entry/"
+
+OK_STATUS = 200
+BASE_COLUMNS = ['id', 'status']
+BATCH_SIZE = 2
+
+def load_pdb_ids_list(path):
+    "Load pdb ids json file as list. The file structure must be a list. for example: ['100D','1BQK','1DP5'...]"
+    
+    with open(path, 'r') as f:
+        data = json.load(f)
+        
+    assert type(data) == list, "The structure of the input json file must be a single list"
+    
+    return data
+
+
+def download_pdb_object(pdb_id):
+    
+    url = PDB_REST_API_DOWNLOAD_URL + pdb_id + '.pdb'
+    response = requests.get(url)
+    
+    return response
+
+def download_entry_object(pdb_id):
+    
+    url = ENTRY_REST_API_DOWNLOAD_URL + pdb_id 
+    response = requests.get(url)
+    
+    return response
+
+def get_sequence_from_pdb_text(pdb_text):
+    "Get the FIRST sequence from a PDB file"
+    
+    pdb_io = StringIO(pdb_text)
+    sequence = ''
+
+    # Parse the PDB file and extract the sequence using SeqIO
+    for record in SeqIO.parse(pdb_io, "pdb-seqres"):
+        sequence = record.seq
+        return sequence
+
+def get_experimental_method_from_entry_object(response):
+    
+    data = response.json()
+    
+    # Extract the experimental method(s)
+    methods = [expt.get("method", "Unknown") for expt in data.get("exptl", [])]
+    
+    # Return the experimental method(s)
+    if methods:
+        return methods[0]  # Return the first method (there can be multiple)
+    else:
+        return "Unknown"
+            
+
+def download_pdbs_data(ids_path,
+                       output_path='test_output.csv',
+                       features=['sequence'],
+                       type='xray',
+                       reset_download=True):
+    
+    ids = load_pdb_ids_list(ids_path)
+    errors = []
+    
+    #reset file
+    if exists(output_path):
+        if reset_download:
+            #reset files with columns for all features
+            with open(output_path, 'w') as f:
+                columns = BASE_COLUMNS +  features
+                f.write(','.join(columns) + '\n') 
+        
+        else:
+            #filter ids that were not already downloaded
+            ids = [id for id in ids \
+                    if id not in pd.read_csv(output_path)['id'].to_list()]
+    
+    buffer = ''
+    
+    for i, pdb_id in tqdm(enumerate(ids), total=len(ids)):
+        
+        buffer += f'{pdb_id}'
+        data = dict()
+        
+        #get entry
+        entry_response = download_entry_object(pdb_id)
+        
+        if entry_response.status_code == OK_STATUS:
+            method = get_experimental_method_from_entry_object(entry_response)
+            data['method'] = method
+        else:
+            data['method'] = "could not reach record"
+        
+        #get pdb as text
+        pdb_response = download_pdb_object(pdb_id)
+        
+        data['status'] = pdb_response.status_code
+        buffer += f",{data['status']}"
+        
+        if pdb_response.status_code == OK_STATUS:
+            
+            
+            pdb_text = pdb_response.text
+            
+            if "sequence" in features:
+                data['sequence'] = get_sequence_from_pdb_text(pdb_text)
+                        
+        
+            #write all acquired data to buffer according to features input order (which is the columns order in  the output csv)
+            for feature in features:
+                buffer += f",{data[feature]}"
+                
+            
+        
+        else:
+            errors.append(pdb_id)
+            
+            
+        
+        buffer += '\n'    
+        
+        #dump buffer to file
+        if i > 0 and i % BATCH_SIZE == 0:
+            
+            with open(output_path, 'a') as f:
+                f.write(buffer)
+            
+            print(f'Wrote batch {i//BATCH_SIZE} out of {len(ids)//BATCH_SIZE} to file')
+            if len(errors) == 0:
+                print('No errors found so far....')
+            else:
+                print(f"Could not download the following ids: {errors}")
+                
+            buffer = ''
+                
+        sleep(1)
+    
+    #dump rest of buffer to file    
+    with open(output_path, 'a') as f:
+                f.write(buffer)
+    
+
+
+
+def main():
+    download_pdbs_data('/home/ofir/ofir_code/crystoper/pdb_db/20240927_pdb_entry_ids.json',
+                       'test_output.csv')
+    
+
+if __name__ == '__main__':
+    main()
