65fda253558ed0d43371aab23ff148c566df3e7b
OR <oregev11@gmail.com>
2024-09-30 17:52:01 +0300

    json utils

--- crystoper/utils/data.py
+++ crystoper/utils/data.py
@@ -1,6 +1,16 @@
 import os
+import json 
 
 def make_dir(path):
     dir_path = os.path.dirname(path)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
+        
+def load_json(path):
+    with open(path, 'r') as f:
+        return json.load(f)
+    
+def dump_json(obj, path):
+    
+    with open(path, 'w') as f:
+        return json.dump(obj, f)
