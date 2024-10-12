02bcb32d327f5bb3b73818a1c57efe3e809fe684
OR <oregev11@gmail.com>
2024-10-12 08:36:36 +0300

    merge entries and entities in parser

--- crystoper/pdb_db/parser.py
+++ crystoper/pdb_db/parser.py
@@ -6,12 +6,18 @@ from tqdm import tqdm
 from ..utils.data import load_json, write_to_csv_in_batches
 from .. import config
 
-
+#features to extract from each Entry json file
 ENTRY_FEATURES_NODES_PATHS = {'struct_method': ('exptl', 0, 'method'),
                               'crystal_method': ('exptl_crystal_grow', 0, 'method'),
                               'ph': ('exptl_crystal_grow', 0, 'p_h'),
                               'temp': ('exptl_crystal_grow', 0, 'temp'),
-                              'pdbx_details': ('exptl_crystal_grow', 0, 'pdbx_details')}
+                              'pdbx_details': ('exptl_crystal_grow', 0, 'pdbx_details'),
+                              'deposit_date': ['rcsb_accession_info', 'deposit_date'],
+                              'revision_date': ['rcsb_accession_info', 'revision_date']}
+
+#features to extract from each Poly Entity json file
+POLY_ENTITY_FEATURES_NODES_PATHS = {'sequence': ['entity_poly', 'pdbx_seq_one_letter_code_can'],
+                                    'poly_type': ['entity_poly', 'rcsb_entity_polymer_type']}
 
 def pdb_json_parser(entries_folder,
                     poly_entities_folder):
@@ -23,25 +29,53 @@ def pdb_json_parser(entries_folder,
         raise ValueError('poly entities folder MUST be passed')
     
     df_entries = parse_entries(entries_folder)
-
-     
-def parse_entries(entries_folder):
+    df_poly_entities = parse_poly_entities(poly_entities_folder)
     
-    query = join(entries_folder, '**', '*.json')
+    df = df_poly_entities.merge(df_entries, on='pdb_id')
+    
+    output_path = config.parsed_pdbs_path
+    df.to_csv(output_path, index=False)
+    
+    print(f"Parsed PDB data was saved to {output_path}")
+     
+def parse_entries(folder):
+    
+    query = join(folder, '**', '*.json')
     
     data = []
     
     print('Parsing Entries...')
     
-    for entry_path in tqdm(glob(query, recursive=True)):
+    for json_path in tqdm(glob(query, recursive=True)):
         
-        pdb_id = Path(entry_path).stem
+        pdb_id = Path(json_path).stem
         
-        entry = load_json(entry_path)
+        entry = load_json(json_path)
         
         data.append([pdb_id] + [get_item(entry, nodes_path) for nodes_path in ENTRY_FEATURES_NODES_PATHS.values()])
         
-    return pd.DataFrame(data, headers=['pdb_id'] + list(ENTRY_FEATURES_NODES_PATHS.keys()))
+    return pd.DataFrame(data, columns=['pdb_id'] + list(ENTRY_FEATURES_NODES_PATHS.keys()))
+
+
+def parse_poly_entities(folder):
+    
+    query = join(folder, '**', '*.json')
+    
+    data = []
+    
+    print('Parsing Poly Entities...')
+    
+    for json_path in tqdm(glob(query, recursive=True)):
+        
+        #poly entity filename format is <PDB-ID>-<POLY-ENTITY-INDEX>.json 
+        pdb_id = Path(json_path).stem.split('-')[0]
+        pe_index = Path(json_path).stem.split('-')[1]
+        
+        pe = load_json(json_path)
+        
+        data.append([pdb_id, pe_index] + [get_item(pe, nodes_path) for nodes_path in POLY_ENTITY_FEATURES_NODES_PATHS.values()])
+        
+    return pd.DataFrame(data, columns=['pdb_id', 'pe_index'] + list(POLY_ENTITY_FEATURES_NODES_PATHS.keys()))
 
 
 def get_item(jsn, nodes_path):
