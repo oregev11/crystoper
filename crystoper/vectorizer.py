8e7bf23d0dde4f1f710abb46b253a7e065ec6ae6
OR <oregev11@gmail.com>
2024-11-06 21:46:20 +0200

    fix

--- crystoper/vectorizer.py
+++ crystoper/vectorizer.py
@@ -143,7 +143,7 @@ class DetailsVectorizer():
             model.to(self.device)
             
             #test model
-            encoder_hidden_states = bart_encode(TEST_SENTENCE, model, tokenizer, device=self.device)
+            encoder_hidden_states = bart_encode(TEST_SENTENCE, model, tokenizer)
             reconstructed_sentence = bart_decode(encoder_hidden_states, model, tokenizer, 'cpu')
             print('SANITY CHECK: encoding and decoding a sentence with BART. the decode sentence should be the same as the original....')
             print(f"Original Sentence:\t{TEST_SENTENCE}")
