99bc0d04bbf0fca17406c7a11fe7f79d0ac7f788
OR <oregev11@gmail.com>
2024-11-06 22:06:14 +0200

    device

--- crystoper/bart.py
+++ crystoper/bart.py
@@ -7,8 +7,7 @@ N_WORDS_IN_DETAILS = 250
 def bart_encode(sentence, model, tokenizer, max_len=N_WORDS_IN_DETAILS, device=None):
 
     inputs = tokenizer(sentence, return_tensors="pt", add_special_tokens=True)
-    inputs['input_ids'] = inputs['input_ids'].to(device)
-    inputs['attention_mask'] = inputs['attention_mask'].to(device)
+    inputs = {key: value.to(device) for key, value in inputs.items()}
     
     # Get the embeddings from the encoder
     with torch.no_grad():
